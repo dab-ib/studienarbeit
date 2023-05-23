fa8554f9e314ec5ac51d89b00cd3858d6beff990
dab-ib <77733201+dab-ib@users.noreply.github.com>
2023-05-23 11:13:25 +0200

    Repository Update
    Repository Update => GitHub Account Sperrung/Disfunktion

--- backend/motiondetection/bsmotiondetector.py
+++ backend/motiondetection/bsmotiondetector.py
@@ -8,10 +8,6 @@ from framebuffer import FrameBuffer
 import cv2
 import asyncio
 import datetime
-"""
-Analyzes Video-Frames using Background-Subtraction Method
-"""
-
 
 class BSMotionDetector(MotionDetector):
     count = 0
@@ -24,7 +20,7 @@ class BSMotionDetector(MotionDetector):
 
     async def _analyze(self, camera: Camera, buffer: FrameBuffer) -> MotionDetectionResult:
         packet = buffer.get_latest_keyframe()
-        if packet == None:
+        if packet is None:
             return MotionDetectionResult(False, 0, camera)
         frames = packet.decode()
         vfs = [x for x in frames if isinstance(x, av.VideoFrame)]
@@ -34,25 +30,22 @@ class BSMotionDetector(MotionDetector):
 
         img = cv2.cvtColor(frame.to_rgb().to_ndarray(), cv2.COLOR_BGR2RGB)
         img = imutils.resize(img, width=600)
-        # Resize to have a width of 500px. Improves speed without sacrificing accuracy
+
         if self.initial_frame is None:
             self.initial_frame = img
             return MotionDetectionResult(False, 0, camera)
 
         dilated_frame = self.backSub.apply(img)
 
-        # Find the contours of the dilated version of the thresholded image
-        contours = cv2.findContours(dilated_frame.copy(), cv2.RETR_EXTERNAL,
-                                    cv2.CHAIN_APPROX_SIMPLE)
+        contours = cv2.findContours(dilated_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = imutils.grab_contours(contours)
+        
         for contour in contours:
-            boxes = []
-            # If contour is smaller than the minimum specified area it does not represent
-            #  significant motion and should thus be ignored
             if cv2.contourArea(contour) < 9000:
                 continue
-            print('mtav')
-            print(frame)
-                # pass
-            # return MotionDetectionResult(True, 5000, camera )
+            
+            Console.out("Motion detected")
+            
+            return MotionDetectionResult(True, 5000, camera)
+
         return MotionDetectionResult(False, 0, camera)
